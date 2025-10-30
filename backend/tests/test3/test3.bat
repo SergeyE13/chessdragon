eef19322213c138507baa9e82f2b3d25460f8256
SergeyE13 <efimov165@yandex.ru>
2025-10-30 13:51:34 +0500

    final version

--- backend/tests/test3/test3.bat
+++ backend/tests/test3/test3.bat
@@ -1,3 +1,4 @@
 rem curl -X POST http://localhost:3000/get-best-move -H "Content-Type: application/json" -d "{\"fen\":\"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[DDdd] w KQkq - 0 1\",\"depth\":10}"
 rem curl -X POST http://localhost:3000/get-best-move -H "Content-Type: application/json" -d "{\"fen\":\"rnbqkbnr/pppppppp/8/8/8/8/PPPPDPPP/RNBQKBNR[Ddd] w KQkq - 0 1\",\"depth\":10}"
-curl -X POST http://localhost:3000/get-best-move -H "Content-Type: application/json"  -d "{\"fen\":\"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[DDdd] w KQkq - 0 1\",\"depth\":10}"
+rem curl -X POST http://localhost:3000/get-best-move -H "Content-Type: application/json"  -d "{\"fen\":\"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[DDdd] w KQkq - 0 1\",\"depth\":10}"
+curl -X POST https://chessdragon.onrender.com:3000/get-best-move -H "Content-Type: application/json"  -d "{\"fen\":\"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[DDdd] w KQkq - 0 1\",\"depth\":10}"
